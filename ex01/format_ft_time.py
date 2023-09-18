1e02bd6abee58e8bbee40e294869df9fc280c578
Kenneth <kennethkhc29@gmail.com>
2023-09-18 15:35:17 +0800

    done ex00 ex01 ex02

---
+++ ex01/format_ft_time.py
@@ -0,0 +1,10 @@
+import time
+
+seconds = time.time()
+formatted_seconds = "{:,.4f}".format(seconds)
+scientific_notation = "{:.2e}".format(float(seconds))
+
+date = time.strftime("%b %d %Y")
+
+print("Seconds since January 1, 1970: " + formatted_seconds + " or " + scientific_notation + " in scientific notation")
+print(date)
