3e4f07eff52402430ba032174e9b6fb10389232d
Kenneth <kennethkhc29@gmail.com>
2023-09-22 18:59:20 +0800

    fixed docs in give_bmi. added slice_me

--- ex00/give_bmi.py
+++ ex00/give_bmi.py
@@ -1,6 +1,27 @@
+def give_bmi(
+    height: list[int | float],
+    weight: list[int | float]
+) -> list[int | float]:
+    """
+    Calculate the bmi by dividing weight in kilograms by
+    height in meters squared.
+    """
+    try:
+        validate_lists(height, weight)
+    except (TypeError, ValueError) as e:
+        error_message = f"Error: {e}" if e else "Error."
+        print(error_message)
+        return None
+    else:
+        bmi_list = [w / h ** 2 for h, w in zip(height, weight)]
+        return bmi_list
+
+
 def validate_lists(height: list, weight: list) -> None:
-    """Validate the lists by checking if they have the same elements,
-if the arguments are ints or floats and if they are positive."""
+    """
+    Validate the lists by checking if they have the same elements,
+    if the arguments are ints or floats and if they are positive.
+    """
     if len(height) != len(weight):
         raise TypeError("Lists do not have the same number of elements.")
     for i in height:
@@ -15,26 +36,11 @@ if the arguments are ints or floats and if they are positive."""
             raise ValueError("Weight should be positive.")
 
 
-def give_bmi(
-    height: list[int | float],
-    weight: list[int | float]
-) -> list[int | float]:
-    """Calculate the bmi by dividing weight in kilograms by
-height in meters squared."""
-    try:
-        validate_lists(height, weight)
-    except (TypeError, ValueError) as e:
-        error_message = f"Error: {e}" if e else "Error."
-        print(error_message)
-        return None
-    else:
-        bmi_list = [w / h ** 2 for h, w in zip(height, weight)]
-        return bmi_list
-
-
 def apply_limit(bmi: list[int | float], limit: int) -> list[bool]:
-    """Checks the bmi against a limit and returns a list of booleans
-for each bmi that is greater than the limit."""
+    """
+    Checks the bmi against a limit and returns a list of booleans
+    for each bmi that is greater than the limit.
+    """
     try:
         limit = int(limit)
         if len(bmi) == 0:
---
+++ ex01/array2D.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+
+def slice_me(family: list, start: int, end: int) -> list:
+    """
+    Prints the shape of a 2D array and slices it according
+    to the start and end indexes passed into the function.
+    """
+    try:
+        if not isinstance(family, list):
+            raise TypeError("Family is not a list!")
+        if len(family) == 0:
+            raise ValueError("List is empty!")
+        for i in family:
+            if not isinstance(i, list):
+                raise TypeError("Not a list of lists!")
+        if start >= len(family):
+            raise ValueError("Starting index is bigger than the list!")
+        array = np.array(family)
+    except (TypeError, ValueError, IndexError) as e:
+        error_message = f"Error: {e}" if e else "Error."
+        print(error_message)
+        return None
+    else:
+        print(f"My shape is : {array.shape}")
+        truncated_array = array[start:end]
+        print(f"My new shape is : {truncated_array.shape}")
+        truncated_list = truncated_array.tolist()
+        return truncated_list
