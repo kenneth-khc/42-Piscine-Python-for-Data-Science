38bbec83292ba0470336a328fd1193aaaf9c5e69
Kenneth <kennethkhc29@gmail.com>
2023-09-24 03:51:26 +0800

    added rotate

--- ex02/load_image.py
+++ ex02/load_image.py
@@ -4,12 +4,15 @@ import numpy as np
 
 
 def ft_load(path: str) -> array:
+    """
+    Loads an image, converts it to RGB and prints its pixel contents.
+    """
     try:
         img = Image.open(path)
         if img.mode != "RGB":
             img = img.convert("RGB")
         pixel_array = np.array(img)
-    except FileNotFoundError as e:
+    except (FileNotFoundError, Exception) as e:
         error_message = f"Error: {e}" if e else "Error."
         print(error_message)
     else:
--- ex03/zoom.py
+++ ex03/zoom.py
@@ -1,21 +1,43 @@
 from load_image import ft_load
+from PIL import Image
 import matplotlib.pyplot as plt
 import numpy as np
-from PIL import Image
+
 
 def main():
-    pixel_data = ft_load("animal.jpeg")
-    zoomed_in = pixel_data[100:500, 450:850]
-    img = Image.fromarray(zoomed_in)
-    img = img.convert("L")
-    greyscaled = np.array(img)
-    two_dim = greyscaled
-    if len(greyscaled.shape) == 2:
-        greyscaled = np.expand_dims(greyscaled, axis=2)
-    print(f"New shape after slicing: {greyscaled.shape} or {two_dim.shape}")
-    print(greyscaled)
-    plt.imshow(greyscaled, cmap="grey")
-    plt.show()
+    """Loads an image, zooms in on it, and prints its new details.
+
+The image loaded converted to an array which is then sliced to zoom
+into a portion of it. The array is then converted back to an image
+to be converted into greyscale. Afterwards, expand another dimension
+on the new array to print its greyscale channel as part of its shape.
+    """
+    try:
+        pixel_data = ft_load("animal.jpeg")
+        if pixel_data is None:
+            raise TypeError("Failed to load image.")
+
+    except TypeError as e:
+        error_message = f"Error: {e}" if e else "Error."
+        print(error_message)
+
+    else:
+        zoomed_in = pixel_data[100:500, 450:850]
+        zoomed_in_image = Image.fromarray(zoomed_in)
+        greyscaled_image = zoomed_in_image.convert("L")
+
+        greyscaled_pixel_data = np.array(greyscaled_image)
+
+        if len(greyscaled_pixel_data.shape) == 2:
+            greyscaled_2d = greyscaled_pixel_data
+            greyscaled_3d = np.expand_dims(greyscaled_pixel_data, axis=2)
+
+        print(f"New shape after slicing: \
+{greyscaled_3d.shape} or {greyscaled_2d.shape}")
+        print(greyscaled_3d)
+        plt.imshow(greyscaled_3d, cmap="grey")
+        plt.show()
+
 
 if __name__ == "__main__":
     main()
